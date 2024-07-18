d90dd70ae5cd4ecbd94bba66bdb6e18aae1ffd0b
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-07-18 19:42:32 +0200

    build: add script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin:$PATH
-export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/aarch64-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin:$PATH
+export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/aarch64-linux-gnu-
+export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
-export PLATFORM_VERSION=12
+export PLATFORM_VERSION=14
+export TARGET_SOC=mt6877
+export TARGET_BUILD_VARIANT=user
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
@@ -14,4 +16,3 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
-
