1a7b8ab3e72e209823a9fcb4ae33a419c48a43d4
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-01-25 13:37:36 +0100

    BACKPORT: get_cred_rcu is required
    Credits to KernelSU-NEXT

--- include/linux/cred.h
+++ include/linux/cred.h
@@ -285,6 +285,17 @@ static inline const struct cred *get_cred(const struct cred *cred)
  * alteration of otherwise immutable credential sets.
  */
 #ifndef CONFIG_KDP_CRED
+static inline const struct cred *get_cred_rcu(const struct cred *cred)
+ {
+	 struct cred *nonconst_cred = (struct cred *) cred;
+	 if (!cred)
+		 return NULL;
+	 if (!atomic_inc_not_zero(&nonconst_cred->usage))
+		 return NULL;
+	 validate_creds(cred);
+	 return cred;
+ }
+
 static inline void put_cred(const struct cred *_cred)
 {
 	struct cred *cred = (struct cred *) _cred;
