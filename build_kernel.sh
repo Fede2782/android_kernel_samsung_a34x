123e01046e97c4a4475b9ac1f48d6db2d2d8f5a8
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-05-08 16:13:27 +0200

    build: update toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin:$PATH
-export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/aarch64-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/clang
+export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin:$PATH
+export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin/aarch64-linux-gnu-
+export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export PLATFORM_VERSION=14
