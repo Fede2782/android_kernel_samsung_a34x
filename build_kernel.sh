afacee1913ce17907cb3b1de9ec411c69012fbe1
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-06-25 14:14:10 +0200

    build_kernel: patch maybe dirty and build date during runtime

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 cd kernel
+
+FTP="
+build/kernel/_setup_env.sh
+build/kernel/kleaf/impl/stamp.bzl
+build/kernel/kleaf/impl/kernel_env.bzl
+"
+
+for f in $FTP; do
+  sed -i "s/SOURCE_DATE_EPOCH=0/SOURCE_DATE_EPOCH\=\\\"\$\(date \+\%s\)\\\"/g" "$f"
+done
+
+sed -i "s/-maybe-dirty//g" "build/kernel/kleaf/impl/stamp.bzl"
+sed -i 's|SOURCE_DATE_EPOCH=0|SOURCE_DATE_EPOCH=\\"$(date +%s)\\"|' "kernel_device_modules-6.6/scripts/gen_build_config.py"
+
 python kernel_device_modules-6.6/scripts/gen_build_config.py --kernel-defconfig mediatek-bazel_defconfig --kernel-defconfig-overlays "sec_ogki_fragment.config mt6877_overlay.config mt6877_teegris_5_overlay.config" --kernel-build-config-overlays "" -m user -o ../out/target/product/a34x/obj/KERNEL_OBJ/build.config
 
 export DEVICE_MODULES_DIR="kernel_device_modules-6.6"
@@ -10,5 +24,6 @@ export DIST_DIR="../out/target/product/a34x/obj/KLEAF_OBJ/dist"
 export DEFCONFIG_OVERLAYS="sec_ogki_fragment.config mt6877_overlay.config mt6877_teegris_5_overlay.config"
 export PROJECT="mgk_64_k66"
 export MODE="user"
+export SOURCE_DATE_EPOCH="$(date +%s)"
 
 ./kernel_device_modules-6.6/build.sh
