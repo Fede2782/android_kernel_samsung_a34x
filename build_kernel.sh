fbb19d9b5d2905f5da849fa28bf21fc459efe473
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-11-03 18:34:22 +0100

    build_kernel: keep normal localversion even when stamp release

--- build_kernel.sh
+++ build_kernel.sh
@@ -13,6 +13,7 @@ for f in $FTP; do
 done
 
 sed -i "s/-maybe-dirty//g" "build/kernel/kleaf/impl/stamp.bzl"
+sed -i "s/stable_scmversion_cmd = _get_status_at_path.*/stable_scmversion_cmd = \"echo \'\'\"/g" "build/kernel/kleaf/impl/stamp.bzl"
 sed -i 's|SOURCE_DATE_EPOCH=0|SOURCE_DATE_EPOCH=\\"$(date +%s)\\"|' "kernel_device_modules-6.6/scripts/gen_build_config.py"
 
 python kernel_device_modules-6.6/scripts/gen_build_config.py --kernel-defconfig mediatek-bazel_defconfig --kernel-defconfig-overlays "mt6877_overlay.config mt6877_teegris_5_overlay.config" --kernel-build-config-overlays "" -m user -o ../out/target/product/a34x/obj/KERNEL_OBJ/build.config
